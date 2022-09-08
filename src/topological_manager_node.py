ef8bf853ac26012aaf215e83a1bc3cbe1e15f7d4
MasakiFujiwara <fmasaki2020@gmail.com>
2022-09-08 16:28:09 +0900

    add pub and sub func

---
+++ src/topological_manager_node.py
@@ -0,0 +1,58 @@
+import rospy
+from std_msgs.msg import Int8MultiArray
+
+
+class topological_node:
+    def __init__(self):
+        rospy.init_node('topological_node', anonymous=True)
+        self.aisle_class_sub = rospy.Subscriber(
+            "/detect_class", Int8MultiArray, self.callback_class, queue_size=1)
+        self.cmd_dir_pub = rospy.Publisher(
+            "cmd_dir_topological", Int8MultiArray, queue_size=1)
+        self.node_no = 1
+        self.count = 0
+        self.cmd_dir = Int8MultiArray()
+        self.cmd_dir.data = (0, 1, 0)
+        self.detect_flag = False
+        self.initialize()
+
+    def initialize(self):
+        pass
+
+    def callback_class(self, data):
+        self.detect_class_data = data.data
+        self.detect_flag = True
+        self.count += 1
+
+    def plan(self, node_no=1):
+        if node_no == 1:
+            if self.count == 1:
+                self.cmd_dir.data = (0, 1, 0)
+            if self.count == 2:
+                self.cmd_dir.data = (0, 0, 1)
+            if self.count == 3:
+                self.cmd_dir.data = (0, 0, 1)
+            if self.count == 4:
+                self.cmd_dir.data = (0, 0, 1)
+            if self.count == 5:
+                self.cmd_dir.data = (1, 0, 0)
+            if self.count == 6:
+                print("stop point")
+        else:
+            pass
+        self.detect_flag = False
+
+    def loop(self):
+        if self.detect_flag:
+            self.plan(self.node_no)
+
+        self.cmd_dir_pub.publish(self.cmd_dir)
+
+
+if __name__ == '__main__':
+    rg = topological_node()
+    DURATION = 0.25
+    r = rospy.Rate(1 / DURATION)
+    while not rospy.is_shutdown():
+        rg.loop
+        r.sleep()
--- src/topological_test.py
+++ src/topological_test.py
@@ -1,28 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
-G2 = nx.Graph([(0, 1), (1, 2), (2, 0)])
 G3 = nx.Graph()
 
 G3.add_nodes_from([1, 2, 3, 4, 5, 6])
 G3.add_edges_from([(1, 2), (1, 4), (2, 1), (2, 3), (2, 5), (3, 2),
                   (3, 6), (4, 5), (5, 4), (5, 6), (6, 5)])
-# G3.add_edges_from([(1, 2, {'weight': 0.6}), (1, 4, {'weight': 0.3}), (2, 1, {'weight': 0.6}), (2, 3, {'weight': 1}), (2, 5, {'weight': 0.3}), (3, 2, {'weight': 1}),
-#                   (3, 6, {'weight': 0.3}), (4, 5, {'weight': 0.6}), (5, 4, {'weight': 0.6}), (5, 6, {'weight': 1}), (6, 5, {'weight': 1})])
+
 pos = {1: (0.0, 0.5), 2: (1.0, 0.5), 3: (3.0, 0.5),
        4: (0.0, 0.0), 5: (1.0, 0.0), 6: (3.0, 0.0)}
 # print(pos)
-
-
-G.add_node(1)       # 一つのノードを追加
-G.add_node('one')
-G.add_nodes_from([2, 3])  # 複数ノードを追加
-G.add_nodes_from(['two', 'three'])
-
-G.add_edge(1, 2)  # エッジの追加
-G.add_edges_from([('one', 1), (2, 3), (2, 4), (2, 'two'),
-                 (2, 'three'), ('two', 'three')])
 
 fig = plt.figure(figsize=(5, 3))
 # ax = fig.add_subplot()
